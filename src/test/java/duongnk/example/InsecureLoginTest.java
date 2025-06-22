c03621339f290a7d3b94eaddeb058d48bcb84128
Khanh Duong <ngkhanhduong2284@gmail.com>
2025-06-22 15:32:57 +0700

    Add initial prọect files

---
+++ src/test/java/duongnk/example/InsecureLoginTest.java
@@ -0,0 +1,19 @@
+package duongnk.example;
+
+import org.junit.jupiter.api.Test;
+
+class InsecureLoginTest {
+    @Test
+    void testLoginSuccess() {
+        InsecureLogin.login("admin","123456");
+    }
+    @Test
+    void testLoginFail() {
+        InsecureLogin.login("user","wrongpassword");
+    }
+    @Test
+    void testPrintUserInfo(){
+        InsecureLogin insecureLogin = new InsecureLogin();
+        insecureLogin.printUserInfo("John Doe");
+    }
+}
